4826ac17551949d979c94333bc9f53f86db9bbb6
jhoover4 <jordan@hoovermld.com>
2018-03-31 23:27:43 -0500

    First draft of mineral_detail view and template

--- mineral_catalog/mineral_detail/urls.py
+++ mineral_catalog/mineral_detail/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.mineral_detail, name='mineral_detail'),
+    path('<int:mineral_id>/', views.mineral_detail, name='mineral_detail'),
 ]
--- mineral_catalog/mineral_detail/views.py
+++ mineral_catalog/mineral_detail/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import render, get_object_or_404
+
+from .models import Mineral
 
 
-def mineral_detail(request, miner):
-    try:
-        get_category = Category.objects.get(name=d['category'])
-    except Category.DoesNotExist:
-        get_category = Category.objects.create(name=d['category'])
-        get_category.save()
+def mineral_detail(request, mineral_id):
+    mineral = get_object_or_404(Mineral, pk=mineral_id)
 
-    return render(request, 'detail.html')
+    class MineralForm(forms.ModelForm):
+        class Meta:
+            model = Mineral
+            fields = ['strunz', 'crystal_system', 'cleavage', 'mohs_scale',
+                      'unit_cell', 'color', 'crystal_symmetry',
+                      'luster', 'streak', 'diaphaneity', 'optical_prop',
+                      'refractive_index', 'crystal_habit',
+                      'specific_gravity', 'group']
+            labels = {
+                'strunz': 'Strunz Classification',
+                'mohs_scale': 'Mohs Scale Hardness'
+            }
+
+    mineral_form = MineralForm(instance=mineral)
+
+    context = {
+        'mineral': mineral,
+        'mineral_form': mineral_form,
+    }
+
+    return render(request, 'detail.html', context)
