db02123f5c83f434c74e480b1ede3a8696c1631c
jhoover4 <jordan@hoovermld.com>
2018-04-01 15:43:33 -0500

    Fixed missing images

--- mineral_catalog/mineral_detail/tests.py
+++ mineral_catalog/mineral_detail/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Mineral, Group, Category
+
+
+class MineralModelTests(TestCase):
+
+    def setUp(self):
+        self.new_mineral.objects.create(name='Test')
+        self.new_mineral.save()
+
+    def test_name(self):
+        """
+        was_published_recently() returns False for questions whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+class IndexViewTests(TestCase):
+    def test_future_question_and_past_question(self):
+
+        response = self.client.get(reverse('index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            ['<Question: Past question.>']
+        )
