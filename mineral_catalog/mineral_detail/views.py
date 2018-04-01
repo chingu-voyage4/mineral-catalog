91fd8ee17ca290a7d9142a179c55966980511a25
jhoover4 <jordan@hoovermld.com>
2018-03-31 22:23:57 -0500

    Registered models with admin.

--- mineral_catalog/mineral_detail/views.py
+++ mineral_catalog/mineral_detail/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 
-def mineral_detail(request):
+def mineral_detail(request, miner):
+    try:
+        get_category = Category.objects.get(name=d['category'])
+    except Category.DoesNotExist:
+        get_category = Category.objects.create(name=d['category'])
+        get_category.save()
+
     return render(request, 'detail.html')
