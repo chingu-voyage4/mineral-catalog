42713f5a6dbc4d6f102a34674a5a825af8e4ad48
jhoover4 <jordan@hoovermld.com>
2018-04-01 22:14:29 -0500

    Seperate form into new file.

--- mineral_catalog/mineral_detail/views.py
+++ mineral_catalog/mineral_detail/views.py
@@ -1,26 +1,13 @@
 import random
 
-from django import forms
 from django.shortcuts import render, get_object_or_404
 
+from .forms import MineralForm
 from .models import Mineral
 
 
 def mineral_detail(request, mineral_id):
     mineral = get_object_or_404(Mineral, pk=mineral_id)
-
-    class MineralForm(forms.ModelForm):
-        class Meta:
-            model = Mineral
-            fields = ['strunz', 'crystal_system', 'cleavage', 'mohs_scale',
-                      'unit_cell', 'color', 'crystal_symmetry',
-                      'luster', 'streak', 'diaphaneity', 'optical_prop',
-                      'refractive_index', 'crystal_habit',
-                      'specific_gravity']
-            labels = {
-                'strunz': 'Strunz Classification',
-                'mohs_scale': 'Mohs Scale Hardness'
-            }
 
     mineral_form = MineralForm(instance=mineral)
 
